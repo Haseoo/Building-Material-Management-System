f64184c46160152c70633fdce402669e33d7f6e5
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-22 22:04:20 +0200

    Add company contact data

--- com.Github.Haseoo.BMMS.Data/Entities/Company.cs
+++ com.Github.Haseoo.BMMS.Data/Entities/Company.cs
@@ -1,11 +1,12 @@
-﻿namespace com.Github.Haseoo.BMMS.Data.Entities
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace com.Github.Haseoo.BMMS.Data.Entities
 {
     public class Company : Entity
     {
         public string Name { get; set; }
         public string Address { get; set; }
-        public string Representative { get; set; }
-        public string EmailAddress { get; set; }
-        public string PhoneNumber { get; set; }
+        public IList<CompanyContactData> ContactData { get; set; }
     }
 }
---
+++ com.Github.Haseoo.BMMS.Data/Entities/CompanyContactData.cs
@@ -0,0 +1,10 @@
+﻿namespace com.Github.Haseoo.BMMS.Data.Entities
+{
+    public class CompanyContactData
+    {
+        public string Description { get; set; }
+        public string Representative { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
