6a099e66840a1f0f2372025cef26bbc55199cbd8
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-22 23:21:59 +0200

    Add repository context

---
+++ com.Github.Haseoo.BMMS.Data/Repositories/RepositoryContext.cs
@@ -0,0 +1,11 @@
+﻿using com.Github.Haseoo.BMMS.Data.Repositories.Ports;
+
+namespace com.Github.Haseoo.BMMS.Data.Repositories
+{
+    public class RepositoryContext
+    {
+        public ICompanyRepository CompanyRepository { get; set; }
+        public IOfferRepository OfferRepository { get; set; }
+        public IMaterialRepository MaterialRepository { get; set; }
+    }
+}
