0adb0db66990255de60e70c2218015ce8183dff4
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-28 00:03:03 +0200

    Rename module

--- com.Github.Haseoo.BMMS.Persistence/Properties/AssemblyInfo.cs
+++ com.Github.Haseoo.BMMS.Persistence/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("com.Github.Haseoo.BMMS.Data")]
+[assembly: AssemblyTitle("com.Github.Haseoo.BMMS.Persistence")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("com.Github.Haseoo.BMMS.Data")]
+[assembly: AssemblyProduct("com.Github.Haseoo.BMMS.Persistence")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
