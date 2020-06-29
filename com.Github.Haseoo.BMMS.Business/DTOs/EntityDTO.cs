6545733f807d138426390df0c60cb6ffa2c2d354
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-29 19:06:48 +0200

    Add dto layer

---
+++ com.Github.Haseoo.BMMS.Business/DTOs/EntityDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace com.Github.Haseoo.BMMS.Business.DTOs
+{
+    public abstract class EntityDTO
+    {
+        protected EntityDTO(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
