848c0b7b5dbe82001540a9b9f3b4afbf3a7865c7
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-30 17:02:57 +0200

    Add material repository test

--- com.Github.Haseoo.BMMS.IntegrationTests/Config/InMemorySessionFactoryProvider.cs
+++ com.Github.Haseoo.BMMS.IntegrationTests/Config/InMemorySessionFactoryProvider.cs
@@ -1,7 +1,49 @@
-﻿namespace com.Github.Haseoo.BMMS.IntegrationTests.Config
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using Entity = com.Github.Haseoo.BMMS.Data.Entities.Entity;
+
+namespace com.Github.Haseoo.BMMS.IntegrationTests.Config
 {
     public class InMemorySessionFactoryProvider
     {
-        
+        private static InMemorySessionFactoryProvider _instance;
+        public static InMemorySessionFactoryProvider Instance => _instance ?? (_instance = new InMemorySessionFactoryProvider());
+
+        private ISessionFactory _sessionFactory;
+        private Configuration _configuration;
+
+        private InMemorySessionFactoryProvider() { }
+
+        public void Initialize()
+        {
+            _sessionFactory = CreateSessionFactory();
+        }
+
+        private ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(SQLiteConfiguration.Standard.InMemory().ShowSql())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Entity>())
+                .ExposeConfiguration(cfg => _configuration = cfg)
+                .BuildSessionFactory();
+        }
+
+        public ISession OpenSession()
+        {
+            var session = _sessionFactory.OpenSession();
+            var export = new SchemaExport(_configuration);
+            export.Execute(true, true, false, session.Connection, null);
+            return session;
+        }
+
+        public void Dispose()
+        {
+            _sessionFactory?.Dispose();
+            _sessionFactory = null;
+            _configuration = null;
+        }
     }
 }
