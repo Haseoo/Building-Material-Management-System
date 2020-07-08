891876de079a350ce69404e26edfad85c891e0bf
Haseoo <dawid_szymkiewicz@hotmail.com>
2020-07-08 19:17:34 +0200

    Add material opertaion dto with it's builder

---
+++ com.Github.Haseoo.BMMS.Business/DTOs/OperationDTOs/MaterialOperationDto.cs
@@ -0,0 +1,45 @@
+﻿namespace com.Github.Haseoo.BMMS.Business.DTOs.OperationDTOs
+{
+    public sealed class MaterialOperationDto
+    {
+        private MaterialOperationDto()
+        {
+        }
+
+        public string Name { get; private set; }
+        public string Specification { get; private set; }
+
+        public class DtoBulder
+        {
+            private string _name;
+            private string _specification;
+
+            public DtoBulder Name(string name)
+            {
+                _name = name;
+                return this;
+            }
+
+            public DtoBulder Specification(string specification)
+            {
+                _specification = specification;
+                return this;
+            }
+
+            public MaterialOperationDto Build()
+            {
+                var dto = new MaterialOperationDto
+                {
+                    Name = _name,
+                    Specification = _specification
+                };
+                return dto;
+            }
+        }
+
+        public static DtoBulder Builder()
+        {
+            return new DtoBulder();
+        }
+    }
+}
