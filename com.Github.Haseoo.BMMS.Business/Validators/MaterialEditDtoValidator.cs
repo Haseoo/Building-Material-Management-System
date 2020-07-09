fe9fd96e33b48efd0f1ebb26bfe819865b91bda9
Haseoo <dawid_szymkiewicz@hotmail.com>
2020-07-10 00:47:38 +0200

    Add material, company add and edit dto validators

---
+++ com.Github.Haseoo.BMMS.Business/Validators/MaterialEditDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using com.Github.Haseoo.BMMS.Business.DTOs.OperationDTOs;
+using com.Github.Haseoo.BMMS.Business.Validators.ErrorMessages;
+using FluentValidation;
+
+namespace com.Github.Haseoo.BMMS.Business.Validators
+{
+    public class MaterialEditDtoValidator : AbstractValidator<MaterialOperationDto>
+    {
+        public MaterialEditDtoValidator()
+        {
+            RuleFor(x => x.Name).
+                NotEmpty()
+                .When(x => x.Name != null)
+                .WithMessage(ErrorMessageGenerator
+                    .GetEmptyValidationErrorMessage(ValidatedEntities.Material));
+        }
+    }
+}
