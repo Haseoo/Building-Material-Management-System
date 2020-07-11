b261aeb9257b5930ce8b2f8737bc6414f290e805
Haseoo <dawid_szymkiewicz@hotmail.com>
2020-07-12 00:12:05 +0200

    Add offer edit dto validator, change type price int offer dto to nullable type

---
+++ com.Github.Haseoo.BMMS.Business/Validators/OfferEditDtoValidator .cs
@@ -0,0 +1,22 @@
+﻿using com.Github.Haseoo.BMMS.Business.DTOs.OperationDTOs;
+using com.Github.Haseoo.BMMS.Business.Validators.ErrorMessages;
+using FluentValidation;
+
+namespace com.Github.Haseoo.BMMS.Business.Validators
+{
+    public class OfferEditDtoValidator : AbstractValidator<OfferOperationDto>
+    {
+        public OfferEditDtoValidator()
+        {
+            RuleFor(x => x.Unit)
+                .NotEmpty()
+                .When(x => x.Unit != null)
+                .WithMessage(ErrorMessageGenerator
+                    .GetEmptyValidationErrorMessage(ValidatedEntities.Offer));
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0M)
+                .When(x => x.UnitPrice != null)
+                .WithMessage("Price must be greater than zero");
+        }
+    }
+}
