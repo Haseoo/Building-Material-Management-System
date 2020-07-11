83ab8ba49d428f345d0d102ff6f479c5a43610c4
Haseoo <dawid_szymkiewicz@hotmail.com>
2020-07-12 00:08:36 +0200

    Edit company edit dto validator, add offer add dto validator

---
+++ com.Github.Haseoo.BMMS.Business/Validators/OfferAddDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using com.Github.Haseoo.BMMS.Business.DTOs.OperationDTOs;
+using com.Github.Haseoo.BMMS.Business.Validators.ErrorMessages;
+using FluentValidation;
+
+namespace com.Github.Haseoo.BMMS.Business.Validators
+{
+    public class OfferAddDtoValidator : AbstractValidator<OfferOperationDto>
+    {
+        public OfferAddDtoValidator()
+        {
+            RuleFor(x => x.Unit)
+                .NotEmpty()
+                .WithMessage(ErrorMessageGenerator
+                    .GetEmptyValidationErrorMessage(ValidatedEntities.Offer));
+            RuleFor(x => x.CompanyId)
+                .NotNull()
+                .WithMessage(ErrorMessageGenerator
+                    .GetEmptyValidationErrorMessage(ValidatedEntities.Offer));
+            RuleFor(x => x.MaterialId)
+                .NotNull()
+                .WithMessage(ErrorMessageGenerator
+                    .GetEmptyValidationErrorMessage(ValidatedEntities.Offer));
+            RuleFor(x => x.UnitPrice)
+                .NotNull()
+                .WithMessage(ErrorMessageGenerator
+                    .GetEmptyValidationErrorMessage(ValidatedEntities.Offer));
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0M)
+                .WithMessage("Price must be greater than zero");
+        }
+    }
+}
