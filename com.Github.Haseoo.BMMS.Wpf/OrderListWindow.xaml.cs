092946227385d01fc881f817883d8ed3e021c599
Haseoo <dawid_szymkiewicz@hotmail.com>
2021-05-01 11:24:50 +0200

    Add ommited methods

--- com.Github.Haseoo.BMMS.Wpf/OrderListWindow.xaml.cs
+++ com.Github.Haseoo.BMMS.Wpf/OrderListWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace com.Github.Haseoo.BMMS.Wpf
 {
@@ -20,6 +21,31 @@ namespace com.Github.Haseoo.BMMS.Wpf
         private void OnCancel(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void OnDataGridKeyPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnEntryEdit(object sender, RoutedEventArgs routedEventArgs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnRefresh(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnShowEntry(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnEntryDelete(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
