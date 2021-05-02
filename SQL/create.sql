2dc380df8de9d78313d42e39439b8651dbd23368
Haseoo <dawid_szymkiewicz@hotmail.com>
2021-05-02 15:31:17 +0200

    Update sql create script

--- SQL/create.sql
+++ SQL/create.sql
@@ -1,5 +1,7 @@
 BEGIN TRAN;
 
+DROP TABLE IF EXISTS ORDER_POSITIONS;
+DROP TABLE IF EXISTS ORDER_LISTS;
 DROP TABLE IF EXISTS OFFERS;
 DROP TABLE IF EXISTS COMPANY_CONTACT_DATA;
 DROP TABLE IF EXISTS COMPANY;
@@ -21,7 +23,9 @@ create table COMPANY_CONTACT_DATA
     REPRESENTATIVE_NAME_AND_SURNAME text             not null,
     EMAIL_ADDRESS                   text             not null,
     PHONE_NUMBER                    text             not null,
-    COMPANY_ID                      uniqueidentifier constraint C_DATA_COMPANY_FK references COMPANIES
+    COMPANY_ID                      uniqueidentifier
+        constraint C_DATA_COMPANY_FK
+            references COMPANIES
 );
 
 create table MATERIALS
@@ -38,8 +42,31 @@ create table OFFERS
     UNIT_PRICE             decimal(10, 2)   not null check ([UNIT_PRICE] >= 0),
     COMMENTS               text,
     LAST_MODIFICATION_DATE datetime,
-    COMPANY_ID             uniqueidentifier not null constraint OFFER_COMPANY_FK references COMPANIES,
-    MATERIAL_ID            uniqueidentifier not null constraint OFFER_MATERIAL_FK references MATERIALS
+    COMPANY_ID             uniqueidentifier not null
+        constraint OFFER_COMPANY_FK
+            references COMPANIES,
+    MATERIAL_ID            uniqueidentifier not null
+        constraint OFFER_MATERIAL_FK
+            references MATERIALS
+);
+
+create table ORDER_LISTS
+(
+    ID                     uniqueidentifier not null primary key,
+    LAST_MODIFICATION_DATE datetime,
+    NAME                   text             not null
+);
+
+create table ORDER_POSITIONS
+(
+    ID            uniqueidentifier not null primary key,
+    QUANTITY      int              not null check ([QUANTITY] > 0),
+    ORDER_LIST_ID uniqueidentifier not null
+        constraint POSITION_ORDER_FK
+            references ORDER_LISTS,
+    MATERIAL_ID   uniqueidentifier not null
+        constraint POSITION_OFFER_FK
+            references OFFERS
 );
 
 COMMIT;
